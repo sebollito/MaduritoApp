77a31e4b888e2b97cb1932c9e83b669e2f1d603d
Camilo Joga <josejogag@live.com>
2023-07-21 04:18:48 -0400

    profile information dynamic

--- src/screens/Profile/ProfileScreen.js
+++ src/screens/Profile/ProfileScreen.js
@@ -1,70 +1,83 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
-import { AuthContext } from '../../context/authContext';
+import React, { useState, useEffect, useContext } from "react";
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  ActivityIndicator,
+} from "react-native";
+import { AuthContext } from "../../context/authContext";
 
 const ProfileScreen = (props) => {
   const { navigation } = props;
-  const { getToken } = useContext(AuthContext);
-  const token = getToken();
-
-  const [userData, setUserData] = useState(null);
+  const { user, getToken, logout } = useContext(AuthContext);
+  const [userData, setUserData] = useState(user);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    fetch('http://cjoga.dyndns-server.com:5000/me', {
-      method: 'GET',
-      headers: {
-        'Authorization': `Bearer ${token}`,
-      },
-    })
-      .then(response => {
+    const fetchUserData = async () => {
+      try {
+        const token = await getToken();
+        const response = await fetch("http://cjoga.dyndns-server.com:5000/me", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
-        return response.json();
-      })
-      .then(data => {
-        // data contains the response from the API
+
+        const data = await response.json();
         setUserData(data);
-      })
-      .catch(error => {
-        console.error('Error:', error.message);
-      });
+        setLoading(false);
+      } catch (error) {
+        console.error("Error:", error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchUserData();
   }, []);
-  
+
   const handleDeleteHistory = () => {
-    // Logic for deleting history
-    console.log('Delete History');
+    // <tbd>
+    console.log("Delete History");
   };
 
   const handleUpdatePassword = () => {
-    // Logic for updating password
-    console.log('Update Password');
     navigation.navigate("ResetPassword");
-
   };
 
   const handleLogout = () => {
-    // Logic for logging out
-    console.log('Logout');
+    logout();
     navigation.navigate("Login");
-
   };
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#63A024" />;
+  }
+
+  if (!userData) {
+    return <Text>Something went wrong...</Text>;
+  }
 
   return (
     <View style={styles.container}>
-      <Image source={require('./profile-picture-test.png')} style={styles.profilePicture} />
-
+      <Image
+        source={require("./profile-picture-test.png")}
+        style={styles.profilePicture}
+      />
       <Text style={styles.name}>{userData.name}</Text>
       <Text style={styles.email}>{userData.email}</Text>
-
       <TouchableOpacity style={styles.button} onPress={handleDeleteHistory}>
         <Text style={styles.buttonText}>Delete My History</Text>
       </TouchableOpacity>
-
       <TouchableOpacity style={styles.button} onPress={handleUpdatePassword}>
         <Text style={styles.buttonText}>Update Password</Text>
       </TouchableOpacity>
-
       <TouchableOpacity style={styles.button} onPress={handleLogout}>
         <Text style={styles.buttonText}>Logout</Text>
       </TouchableOpacity>
@@ -75,8 +88,8 @@ const ProfileScreen = (props) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     padding: 20,
   },
   profilePicture: {
@@ -87,7 +100,7 @@ const styles = StyleSheet.create({
   },
   name: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
   },
   email: {
@@ -95,14 +108,14 @@ const styles = StyleSheet.create({
     marginBottom: 20,
   },
   button: {
-    backgroundColor: '#63A024',
+    backgroundColor: "#63A024",
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5,
     marginBottom: 10,
   },
   buttonText: {
-    color: 'white',
+    color: "white",
     fontSize: 16,
   },
 });
